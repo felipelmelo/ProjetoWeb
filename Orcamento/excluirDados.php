d53bcef347d3950edd6628dc90a17832748b154b
Waleska Dias <waleska.dsouza@gmail.com>
2014-08-02 23:34:28 -0300

    Alteração na estrutura do projeto.
    Signed-off-by: Waleska Dias <waleska.dsouza@gmail.com>

--- Orcamento/excluirDados.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	require_once 'repositorioEstabelecimento.php';
-	
-	$estabelecimento = RepositorioEstabelecimento::getInstancia()->excluir($_GET['id']);
-	header("Location : exibirDados.php");
-?>
